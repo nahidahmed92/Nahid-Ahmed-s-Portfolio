39414515b1db06adf1d9a0d067ea90dd8cd45a9b
Nahid Ahmed <npahmed92@gmail.com>
2024-05-09 22:15:15 -0400

    refactor Home page from Header to its own page

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,40 @@
+export default function About() {
+  return (
+    <div
+      className="tab-pane fade show active"
+      id="home-tab-pane"
+      role="tabpanel"
+      aria-labelledby="home-tab"
+      tabIndex="0">
+      <h1>About Me</h1>
+      <img
+        alt="my recent photo"
+        id="me"
+        src="../../assets/my-photo.jpeg"
+        style={{
+          float: 'left',
+          maxWidth: '20%',
+          marginRight: '20px',
+        }}
+      />
+      <p>
+        HOME Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, molestiae non?
+        Quibusdam at sapiente, ipsum illum necessitatibus odit veniam enim odio provident, saepe
+        fugiat recusandae optio voluptates, consequatur deserunt amet? Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Error odit sunt architecto reprehenderit rem corporis, eaque
+        voluptate eum perspiciatis ipsam dicta. Sequi omnis, earum illo numquam accusantium iure
+        quia exercitationem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        laborum sit rerum odio obcaecati eveniet veritatis magni, beatae sapiente similique earum
+        voluptas tenetur doloribus eos quaerat, quam deleniti error autem? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Alias assumenda consequuntur possimus quidem iste, totam,
+        ipsum pariatur praesentium, nisi illo aspernatur? Atque, modi praesentium aperiam facere
+        alias hic animi porro? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam quia
+        ipsam enim? Id nulla ullam, quae sunt nemo molestias, sapiente facilis dolorum fugit odio
+        magni eos quod, laudantium a odit? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Veniam dignissimos, temporibus sapiente ad in consectetur repellendus, voluptas laudantium
+        adipisci laborum, repudiandae porro ducimus quis dolore maiores laboriosam magnam ea vero?
+        HOME
+      </p>
+    </div>
+  );
+}
