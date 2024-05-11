6571f7f029517516d494cdbe2be118cf718b583b
Nahid Ahmed <npahmed92@gmail.com>
2024-05-11 00:37:33 -0400

    contact form event handlers for input change and submisison

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,41 +1,86 @@
+import { useState } from 'react';
+
 export default function Contact() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('user submission', form);
+
+    // Reset form after submission
+    setForm({
+      name: '',
+      email: '',
+      message: '',
+    });
+  };
+
   return (
-    <div className="border border-1 border-light rounded p-4 w-50">
-      <form>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">
-            Name
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleFormControlInput1"
-            placeholder="John Doe"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">
-            Email Address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlTextarea1" className="form-label">
-            Message
-          </label>
-          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-        <div className="text-center">
-          <button type="submit" className="btn btn-secondary">
-            Submit
-          </button>
-        </div>
-      </form>
+    <div
+      className="d-flex flex-column tab-pane fade show active w-50"
+      id="contact-tab-pane"
+      role="tabpanel"
+      aria-labelledby="contact-tab"
+      tabIndex="0">
+      <h1>Contact</h1>
+      <div className="border border-1 border-light rounded p-4">
+        <form className="contactForm" onSubmit={handleFormSubmit}>
+          <div className="mb-3">
+            <label htmlFor="nameInput" className="form-label">
+              Name
+            </label>
+            <input
+              type="name"
+              className="form-control"
+              id="nameInput"
+              name="name"
+              onChange={handleInputChange}
+              placeholder="John Doe"
+              value={form.name}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="emailInput" className="form-label">
+              Email Address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="emailInput"
+              name="email"
+              onChange={handleInputChange}
+              placeholder="name@example.com"
+              value={form.email}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="messageInput" className="form-label">
+              Message
+            </label>
+            <textarea
+              className="form-control"
+              id="messageInput"
+              name="message"
+              onChange={handleInputChange}
+              rows="3"
+              value={form.message}></textarea>
+          </div>
+          <div className="text-center">
+            <button type="submit" className="btn btn-secondary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
