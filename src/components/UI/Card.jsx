8649377777fcfb0928b9fdcbbb57bf5a34a70cd0
Nahid Ahmed <npahmed92@gmail.com>
2024-05-12 00:34:55 -0400

    add hover style to live link and add comments

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -5,7 +5,10 @@ import socialNetwork from '../../../assets/portfolio/social-network-api.png';
 import techBlog from '../../../assets/portfolio/tech-blog.png';
 
 export default function Card({ cards }) {
+  // checks if card is hovered
   const [isHovered, setIsHovered] = useState(false);
+  // chekcs if deployment link is hovered
+  const [isHoveredLive, setIsHoveredLive] = useState(false);
   const { title, link, repo, description } = cards;
   const cardImages = {
     PWA: pwa,
@@ -13,48 +16,56 @@ export default function Card({ cards }) {
     'Tech Blog': techBlog,
   }[title];
 
+  // sets the card style when hovered
   const overlayStyle = {
     display: isHovered ? 'flex' : 'none',
     backgroundColor: isHovered ? 'rgba(0, 0, 0, 0.5)' : 'transparent',
     transition: 'background-color 0.3s ease',
   };
 
-  const linkStyle = {
-    textDecoration: isHovered ? 'none' : 'underline',
+  // sets the live link style when hovered
+  const liveLinkStyle = {
+    border: isHoveredLive ? '1px solid #dee2e6' : '',
+    borderRadius: isHoveredLive ? '0.25rem' : '',
   };
 
-  /**
-   * dynamically add:
-   * h5 title - title of project
-   * link
-   * description
-   */
+  // certain apps do not have a live deployment link, set no border style
+  if (link === null) {
+    liveLinkStyle.border = '';
+  }
 
   return (
     <div
-      // className=" card text-bg-secondary justify-content-center col-12 col-md-6 col-lg-4 m-3"
-      className=" card text-bg-secondary justify-content-center col-4 m-3"
+      className="card text-bg-secondary justify-content-center"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      // style={{
-      //   backgroundColor: 'blue',
-      // }}
-    >
+      style={{
+        height: '100%',
+        objectFit: 'cover',
+        // add custom backgroundColor here and remove text-bg-secondary
+        // backgroundColor: 'powderblue',
+      }}>
       {cardImages && <img src={cardImages} className="card-img" alt={title} />}
       <div className="card-img-overlay flex-column justify-content-end" style={overlayStyle}>
         <div className="d-flex flex-row justify-content-center align-items-center mb-3">
           <h5 className="card-title px-3 justify-content-end">
-            <a className="text-decoration-none text-light" href={link} target="_blank">
+            <a
+              className="text-decoration-none text-light p-2"
+              href={link}
+              onMouseEnter={() => setIsHoveredLive(true)}
+              onMouseLeave={() => setIsHoveredLive(false)}
+              style={liveLinkStyle}
+              target="_blank">
               {title}
             </a>
           </h5>
           <a href={repo} target="_blank">
             <GitHubLogo
-              width="35"
-              height="35"
-              fill="white"
               className="logo mx-2"
               aria-label="Github logo"
+              fill="white"
+              height="35"
+              width="35"
             />
           </a>
         </div>
