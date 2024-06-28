0de2effd50b41d79330115d336cd143627a8d145
Nahid Ahmed <npahmed92@gmail.com>
2024-06-28 16:01:23 -0400

    add font and styling to home page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -7,24 +7,34 @@ export default function About() {
       id="home-tab-pane"
       role="tabpanel"
       aria-labelledby="home-tab"
+      style={{ width: '100%' }}
       tabIndex="0">
       <h1>About Me</h1>
-      <img
-        alt="my recent photo"
-        id="me"
-        src={myPhoto}
-        style={{
-          float: 'left',
-          maxWidth: '20%',
-          marginRight: '20px',
-        }}
-      />
-      <p>
-        I'm a driven individual who has a strong passion for QA Automation. Although I didn't
-        initially pursue this field after completing my college education, I made a deliberate
-        career change because of my deep enthusiasm for it. I look forward to starting my day
-        eagerly anticipating in the work I will contribute.
-      </p>
+      <div className="row my-5 pe-5">
+        <div className="col-12 col-md-6 order-md-1 d-flex justify-content-center">
+          <img
+            alt="my recent photo"
+            className="img-fluid w-50"
+            id="me"
+            src={myPhoto}
+            // style={{ width: '60%' }}
+          />
+        </div>
+        <div className="col-12 col-md-6 order-md-2">
+          <p
+            className="fs-2"
+            style={{
+              fontFamily: '"Bebas Neue", sans-serif',
+              fontWeight: '400',
+              fontStyle: 'normal',
+            }}>
+            I'm a driven individual who has a strong passion for QA Automation. Although I didn't
+            initially pursue this field after completing my college education, I made a deliberate
+            career change because of my deep enthusiasm for it. I look forward to starting my day
+            eagerly anticipating in the work I will contribute.
+          </p>
+        </div>
+      </div>
       <style>
         {`
       @media (min-width: 768px) {
