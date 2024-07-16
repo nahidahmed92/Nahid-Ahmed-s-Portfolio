d70b124416c7cf2578c1629a48c81ed43efe75d4
Nahid Ahmed <npahmed92@gmail.com>
2024-07-16 01:07:33 -0400

    add production environment

--- server/server.js
+++ server/server.js
@@ -1,30 +1,28 @@
+const cors = require('cors');
 const express = require('express');
+const path = require('path');
 
 const routes = require('./routes');
 const sequelize = require('./config/connection.js');
-const Contact = require('./models/Contact.js');
-const contactRoutes = require('./routes/api/contactRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use(routes);
 
-app.post('/api/contacts', async (req, res) => {
-  const { name, email, phone, message } = req.body;
-  try {
-    const newContact = await Contact.create({ name, email, phone, message });
-    res.status(200).json(newContact);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
 
 sequelize
   .authenticate()
