ca3f3d4251d41406cb4dc9bffa3b24277a0961cd
Nahid Ahmed <npahmed92@gmail.com>
2024-07-16 02:02:37 -0400

    update production path

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
 
