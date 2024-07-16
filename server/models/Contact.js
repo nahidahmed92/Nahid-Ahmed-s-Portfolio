ce9114d6e38aea8abeae295fe396e95891d53cbf
Nahid Ahmed <npahmed92@gmail.com>
2024-07-15 23:08:54 -0400

    create contact model

---
+++ server/models/Contact.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Contact extends Model {}
+
+Contact.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'contact',
+  }
+);
+
+module.exports = Contact;
