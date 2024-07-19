c2d842339b2b1911e3b06d0865555ed277fcb1fe
Nahid Ahmed <npahmed92@gmail.com>
2024-07-19 18:13:05 -0400

    add port settings in connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,7 +6,6 @@ console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_URL:', process.env.DB_URL);
 
 let sequelize;
@@ -17,6 +16,7 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    port: process.env.DB_PORT,
   });
 }
 
