d42c89f47f2216e6428dd3c7950fe4b61f4b91b8
Nahid Ahmed <npahmed92@gmail.com>
2024-07-20 01:48:33 -0400

    send email notification api route

--- server/routes/api/contactRoutes.js
+++ server/routes/api/contactRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Contact = require('../../models/Contact');
+const sendEmail = require('../../mailer');
 
 router.get('/', async (req, res) => {
   try {
@@ -14,6 +15,14 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const contactData = await Contact.create(req.body);
+
+    // send email notification
+    await sendEmail(
+      process.env.EMAIL_USER,
+      `New Contact Form Submission`,
+      `Hello,\n\n${contactData.message}\n\nThank You,\n\n${contactData.name}\n${contactData.email}\n${contactData.phone}`
+    );
+
     res.status(200).json(contactData);
   } catch (err) {
     res.status(400).json(err);
