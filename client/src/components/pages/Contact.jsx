2e794283144425a6b17c4f6d7cbe8f0256128c26
Nahid Ahmed <npahmed92@gmail.com>
2024-07-31 16:11:49 -0400

    update to handle cors

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -67,7 +67,7 @@ export default function Contact() {
     if (valid) {
       console.log('Form submitted:', form);
       try {
-        const response = await fetch('https://nahid-ahmed-s-portfolio.vercel.app' + '/api/contacts', {
+        const response = await fetch('/api/contacts', {
           method: 'POST',
           body: JSON.stringify(form),
           headers: {
