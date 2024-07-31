965bed410dfd8405ea971f581ab340adbfde9ddc
Nahid Ahmed <npahmed92@gmail.com>
2024-07-31 15:21:29 -0400

    edit corsOptions

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: 'http://nahidahmed.com',
-  methods: 'POST',
+  origin: 'https://nahidahmed.com',
+  methods: ['POST'],
 };
 
 // Routes
