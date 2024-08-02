322c32e48faf5682072c00e4a4fbf2911db28e3e
Nahid Ahmed <npahmed92@gmail.com>
2024-08-02 01:35:29 -0400

    update server

--- api/index.js
+++ api/index.js
@@ -9,42 +9,45 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(
-  cors({
-    origin: 'https://nahidahmed.com',
-    methods: ['POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: 'https://nahidahmed.com',
+//     methods: ['POST'],
+//     allowedHeaders: ['Content-Type'],
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 // Routes
 // app.use(routes);
 app.get('/', (req, res) => res.send('Express on Vercel'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//   });
+// }
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connected...');
-    return sequelize.sync({ force: false, alter: true });
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on http://${process.env.DB_HOST}:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on http://${process.env.DB_HOST}:${PORT}`);
+});
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Database connected...');
+//     return sequelize.sync({ force: false, alter: true });
+//   })
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server running on http://${process.env.DB_HOST}:${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error('Unable to connect to the database:', err);
+//   });
 
 module.exports = app;
