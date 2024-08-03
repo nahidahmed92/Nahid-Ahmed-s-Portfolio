1a640be62d44641d27be66d3c996aa02d4d2c4b6
Nahid Ahmed <npahmed92@gmail.com>
2024-08-02 23:43:45 -0400

    add require('pg') to dialect for db_url

--- server/config/connection.js
+++ server/config/connection.js
@@ -11,7 +11,9 @@ console.log('DB_URL:', process.env.DB_URL);
 let sequelize;
 
 if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialectModule: require('pg'),
+  });
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
