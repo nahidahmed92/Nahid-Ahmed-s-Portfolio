d0a9270d54d9ba02c96beea5cbc24151d66da93b
Nahid Ahmed <npahmed92@gmail.com>
2024-08-02 23:53:44 -0400

    comment app.get from production

--- server/server.js
+++ server/server.js
@@ -30,9 +30,9 @@ app.use('/', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  // });
 }
 
 sequelize
