1c3e6eeaf9a6d9355eca1cf9a253efe7120f81a7
Nahid Ahmed <npahmed92@gmail.com>
2024-08-02 23:23:23 -0400

    use sequelize

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 // const path = require('path');
 
 // const routes = require('./routes');
-// const sequelize = require('./config/connection.js');
+const sequelize = require('./config/connection.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
